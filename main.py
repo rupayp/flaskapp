680cbd5ea065d95a29ca5078a1cfd22fd343fa23
Nirjhar K Paul <rupay.p1989@gmail.com>
2020-11-19 12:42:37 +0530

    updated proc file

--- main.py
+++ main.py
@@ -167,3 +167,5 @@ api.add_resource(TaskListAPI, '/api/v1.0/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/api/v1.0/tasks/<int:id>', endpoint='task')
 api.add_resource(PreditAPI, '/api/v1.0/predict', endpoint='predict')
 
+if __name__ == "__main__":
+    app.run()
